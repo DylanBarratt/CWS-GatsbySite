d0b9599c578577df371b7a3a332292bbfdc6cb64
Dylan Barratt <dylanbarratt03@gmail.com>
2021-02-16 14:52:07 +0000

    Added support for video posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,11 +6,40 @@ import Layout from "../components/layout"
 
 import style from "./post.module.css"
 
+function PreviewImage(data) {
+  return (
+    <img
+      src={data.data.data.post.previewImage.file.url}
+      alt="{data.post.title}"
+    />
+  )
+}
+
+function PreviewVideo(data) {
+  return (
+    <iframe
+      title={data.data.data.post.videoUrl}
+      width="560"
+      height="315"
+      src={"https://www.youtube.com/embed/" + data.data.data.post.videoUrl}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  )
+}
+
+function Preview(data) {
+  if (data.data.post.videoUrl == undefined) {
+    return <PreviewImage data={data} />
+  } else {
+    return <PreviewVideo data={data} />
+  }
+}
 const Post = ({ data }) => {
   return (
     <Layout>
       <div className={style.postMain}>
-        <img src={data.post.previewImage.file.url} alt="{data.post.title}" />
+        <Preview data={data} />
         <div className={style.title}>
           <h1>{data.post.title}</h1>
           <h2>
@@ -18,7 +47,9 @@ const Post = ({ data }) => {
             <Link to="/about">By Dr. Catherine O'Leary</Link>
           </h2>
         </div>
-        <p>{documentToReactComponents(JSON.parse(data.post.postBody.raw))}</p>
+        <div>
+          {documentToReactComponents(JSON.parse(data.post.postBody.raw))}
+        </div>
       </div>
     </Layout>
   )
@@ -30,6 +61,7 @@ export const pageQuery = graphql`
   query($slug: String!) {
     post: contentfulBlogPost(slug: { eq: $slug }) {
       dateCreated(formatString: "DD/MM/YYYY")
+      videoUrl
       title
       previewImage {
         file {
